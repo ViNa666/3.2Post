fd6042c2e8ca99e9e18fa3056f4c65091ef01e92
ViNa666 <legenda-2000@yandex.ru>
2022-02-06 14:34:45 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String phone;
+    public String passport;
+    public FormDate birthday = new FormDate();
+    public boolean subscription;
+}
+
